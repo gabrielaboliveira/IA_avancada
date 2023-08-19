b3a1e438fa5cc51841513830647e343f05b348df
Gabriela Oliveira <69415386+gabrielaboliveira@users.noreply.github.com>
2023-08-19 19:57:49 -0300

    Update pdb.cc
    testa se a precondicao = variavel,
    unordered set

--- List_4/fast-downward/src/search/planopt_heuristics/pdb.cc
+++ List_4/fast-downward/src/search/planopt_heuristics/pdb.cc
@@ -60,6 +60,9 @@ PatternDatabase::PatternDatabase(const TNFTask &task, const Pattern &pattern)
 
     int i = 0 ; //coloquei esse contador pra percorrer o distances, nao sei se esta certo
     int count = 0;
+    std::unordered_set<int> operatorclosed; 
+
+
     while (!queue.empty()) { //a condicao tambem poderia ser enquanto o distances nao esta preenchido
       cout<<"======NOVO LOOP DA OPEN======"<<endl;
       QueueEntry entry = queue.top();
@@ -85,14 +88,15 @@ PatternDatabase::PatternDatabase(const TNFTask &task, const Pattern &pattern)
           int pre_conditions_test = 0; 
 
           //percorre as componentes que afetam cada variavel
-          cout<< "OPERADOR INITIAL" << endl;
+          cout<< "OPERADOR INITIAL " << entry.name<< endl;
           for (const auto &tk : entries)
           {
             
             //usa o proprio variable id pra acessar a variavel dentro do state
             cout<<" VAR ID " << tk.variable_id<< " effect "<<tk.effect_value << " precond "<<tk.precondition_value <<"|| index " << index << endl;
             cout<<"  IF efeito " << tk.effect_value<< " = "<<state_old[tk.variable_id] << " stated em var id "<< endl;
-            if (tk.effect_value==state_old[tk.variable_id]){
+            //Mais um teste pra ver se a variavel ja nao e igual a pre condicao, ai nao precisa inserir
+            if (tk.effect_value==state_old[tk.variable_id] && state_new[tk.variable_id] != tk.precondition_value){
               state_new[tk.variable_id] = tk.precondition_value;
               pre_conditions_test++;
               }
@@ -100,10 +104,13 @@ PatternDatabase::PatternDatabase(const TNFTask &task, const Pattern &pattern)
           }
           cout<< "OPERADOR END" <<endl;
           
-          cout<< "  estado gerado " << state_new<< endl;
+          
+          QueueEntry state_queue = {(lookup_distance(state_new) + entry.cost), projection.rank_state(state_new)};
+          cout<< "  estado gerado " << state_queue.second << endl;
           //insere o estado gerado na open
-          if(pre_conditions_test>=1){
-            queue.push({(lookup_distance(state_new) + entry.cost), projection.rank_state(state_new)});
+          if(pre_conditions_test>=1 && operatorclosed.find(state_queue.second)==operatorclosed.end()){
+            operatorclosed.insert(state_queue.second); //coloquei um unordered set pra testar se ja nao foi inserido, nao sei se ta certo assim
+            queue.push(state_queue);
             cout<<"-------novo estado inserido----------"<<endl;
             cout<<endl;
             //reseta o estado e vai para o prox operador
@@ -114,8 +121,8 @@ PatternDatabase::PatternDatabase(const TNFTask &task, const Pattern &pattern)
           //problema: ele gera o estado da forma certa aparentemente, mas parece estar usando sempre o mesmo operador
           
           //break de teste
-          // if(count==4){
-          //   break;
+          //  if(count==5){
+          //    break;
           // }
         }
         cout<<endl;
